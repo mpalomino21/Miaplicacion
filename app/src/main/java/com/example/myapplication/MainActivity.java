f971f777d45ebd6eb244772f5a0bebe36cbe2f49
unknown <Admin@IHR80PBF181.continental.edu.pe>
2019-11-12 22:30:18 -0500

    fina clases

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,47 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.provider.Settings;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+private Button btnProducts;
+private Button btnAboutUs;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            btnProducts=(Button)findViewById(R.id.btnEnter);
+            btnAboutus=(Button)findViewById(R.id.btnAboutUs);
+            btnProducts.setOnClicklistener(new View.OnClickListener(){
+                public void onClick(View v){
+                    log.i(Settings.INFO, "Products");
+                    Inten i=new Intent(MainActivity.this,ProductListAtivity.class);
+                    startActivity(i);
+
+                }
+
+            });
+            btnAboutUs.setOnClicklistener(new View.OnClickListener(){
+                public void onClick(View v){
+                    log.i(Settings.INFO, "Abouts us");
+                    Inten i=new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(i);
+
+
+                }
+
+
+            });
+
+
+        }
+
     }
     @Override
     protected void onResume(){
